e1c4ee73fa11514edee28db2b54f2978e5143737
liujin <liujin@imscv.com>
2017-01-21 16:32:45 +0800

    文件重新建立回来

--- EPC-Lidar_DMA串口能与上位机通信/2D-Lidar/MDK-ARM/2D-Lidar/sysconfig.d
+++ EPC-Lidar_DMA串口能与上位机通信/2D-Lidar/MDK-ARM/2D-Lidar/sysconfig.d
@@ -8,7 +8,7 @@
 2d-lidar\sysconfig.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h
 2d-lidar\sysconfig.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f302xc.h
 2d-lidar\sysconfig.o: ../Drivers/CMSIS/Include/core_cm4.h
-2d-lidar\sysconfig.o: F:\win7\keil\ARM\ARMCC\bin\..\include\stdint.h
+2d-lidar\sysconfig.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
 2d-lidar\sysconfig.o: ../Drivers/CMSIS/Include/core_cmInstr.h
 2d-lidar\sysconfig.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
 2d-lidar\sysconfig.o: ../Drivers/CMSIS/Include/core_cmFunc.h
@@ -16,7 +16,7 @@
 2d-lidar\sysconfig.o: ../Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h
 2d-lidar\sysconfig.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h
 2d-lidar\sysconfig.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-2d-lidar\sysconfig.o: F:\win7\keil\ARM\ARMCC\bin\..\include\stdio.h
+2d-lidar\sysconfig.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
 2d-lidar\sysconfig.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h
 2d-lidar\sysconfig.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h
 2d-lidar\sysconfig.o: ../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h
